ad62e3360e650051d1835b8684c6c69a68ac025f
lihell <lhellwage@t-h.de>
2023-01-24 14:31:31 +0100

    Begin Dashboard

---
+++ src/Frontend/Dashboard.tsx
@@ -0,0 +1,12 @@
+import "./CSS/Dashboard.css"
+import React from "react";
+
+function Dashboard(){
+    return(
+        <div className="Dashboard">
+
+        </div>
+    );
+}
+
+export default Dashboard;
