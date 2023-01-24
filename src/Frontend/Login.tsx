4f0f1efd46e22550d6abb4ec5840313dc02ff099
lihell <lhellwage@t-h.de>
2023-01-24 14:27:04 +0100

    Begin Login Page

---
+++ src/Frontend/Login.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./CSS/Login.css";
+
+function Login() {
+    return(
+        <div className="loginPage">
+            <header className="loginHeader">
+                <h2>Log In</h2>
+            </header>
+            <div className="LoginSquare">
+                <h4>User</h4>
+                <input type="text"/>
+                <h4>Password</h4>
+                <input type="password"/>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
