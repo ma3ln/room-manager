7b0d9c157035b2e0355a9dc61a961c648aeafd2c
Marlon Nobis <nobis222@gmail.com>
2023-01-26 14:34:39 +0100

    Added Routing

--- src/Frontend/App.tsx
+++ src/Frontend/App.tsx
@@ -3,13 +3,18 @@ import './CSS/App.css';
 import Login from './Login';
 import Dashboard from "./Dashboard";
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <Dashboard/>
+          <BrowserRouter>
+              <Routes>
+                  <Route path={"/"} element={<Login />}></Route>
+                  <Route path={"dashboard"} element={<Dashboard />}></Route>
+              </Routes>
+          </BrowserRouter>
       </header>
     </div>
   );
