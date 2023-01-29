a31614f1c851fbbaf6b3bc373a1cfda725edb460
Justus Marx <justus.marx@gmx.de>
2023-01-29 18:13:35 +0100

    Added Restful connection with login page

--- src/Frontend/App.tsx
+++ src/Frontend/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import logo from '../logo.svg';
 import './CSS/App.css';
 import Login from './Login';
 import Dashboard from "./Dashboard";
@@ -6,11 +7,9 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
+
   return (
     <div className="App">
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
       <header className="App-header">
           <BrowserRouter>
               <Routes>
--- src/Frontend/Login.tsx
+++ src/Frontend/Login.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./CSS/Login.css";
 import {Button} from "@mui/material";
 import {Box} from "@mui/material";
@@ -10,12 +10,37 @@ import {Lock} from "@mui/icons-material";
 import Dashboard from "./Dashboard";
 import {Link, useNavigate} from "react-router-dom";
 
+
 function Login() {
+
+    const [postId, setPostId] = useState(0);
+
 
     const navigate = useNavigate();
 
     function handleClick() {
-        navigate("/dashboard");
+/*        //navigate("/dashboard");
+
+        fetch('http://localhost:8081/login', {
+            mode: "cors",
+            method: "POST",
+            body: JSON.stringify({user: 'User123', password: '123456'})
+        }).catch((error) => {
+            console.log(error)
+        });*/
+        const formdata = new FormData();
+        formdata.append("user", "TestUser1");
+        formdata.append("password", "123456");
+
+
+        fetch("http://localhost:8081/login", {
+            method: 'POST',
+            body: formdata,
+            redirect: 'follow'
+        })
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
     }
 
     return(
