74497f68d7230c3f29fe2540b1d25142e8740b73
lihell <lhellwage@t-h.de>
2023-02-06 10:21:28 +0100

    login swipe buttons work now

--- src/Frontend/App.tsx
+++ src/Frontend/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './CSS/App.css';
 import Login from './Login';
 import Dashboard from "./Dashboard";
+import NewRooms from "./NewRooms";
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Sidebars from "./Sidebars";
@@ -14,6 +15,7 @@ function App() {
               <Routes>
               <Route path={"/"} element={<Login />}></Route>
               <Route path={"dashboard"} element={<Dashboard />}></Route>
+              <Route path={"newroom"} element={<NewRooms />}></Route>
               </Routes>
           </BrowserRouter>
     </div>
---
+++ src/Frontend/NewRooms.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewRooms() {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default NewRooms;
